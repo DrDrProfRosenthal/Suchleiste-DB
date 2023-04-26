f49a046049ce78db364cb6a461350a352ad9b35b
ahab64 <merlin.burbach@gmx.de>
2023-04-26 16:07:26 +0200

    03 Backend

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const bankAccountsRouter = require('./routes/bankAccounts');
+
+const app = express();
+
+// Parse JSON request bodies
+app.use(bodyParser.json());
+
+// Mount the bank accounts router at the '/bank-accounts' endpoint
+app.use('/bank-accounts', bankAccountsRouter);
+
+// Start the server on port 3000
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
+});
