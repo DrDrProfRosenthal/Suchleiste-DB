0b4265dd16f3841ba56cfb80cc41d044af05b338
ahab64 <merlin.burbach@gmx.de>
2023-05-02 10:10:01 +0200

    05 Changes to Fe and Be and Readme.md

--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -2,20 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-
-
+import { NavbarComponent } from './navbar/navbar.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [
-    HeaderComponent,
-    FooterComponent
-  ],
-  imports: [
-    CommonModule
-  ],
-  exports: [
-    HeaderComponent,
-    FooterComponent
-  ]
+  declarations: [HeaderComponent, FooterComponent, NavbarComponent],
+  imports: [CommonModule, RouterModule],
+  exports: [HeaderComponent, FooterComponent, NavbarComponent],
 })
-export class SharedModule { }
+export class SharedModule {}
